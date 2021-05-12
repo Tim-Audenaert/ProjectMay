5e1418ffaabc5ee4882022624d975ea3e3fc92ea
Tim-Audenaert <tim.audenaert@hotmail.be>
2021-05-12 11:57:54 +0200

    Researched Required TextBox code

---
+++ ProjectMay/LabelWithRequiredInfo.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace ProjectMay
+{
+    //SOURCE: https://stackoverflow.com/questions/33256045/wpf-validation-depending-on-required-not-required-field/33385575
+    public class LabelWithRequiredInfo : Label
+    {
+        public bool IsRequired
+        {
+            get { return (bool)GetValue(IsRequiredProperty); }
+            set { SetValue(IsRequiredProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for IsRequired.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty IsRequiredProperty =
+            DependencyProperty.Register("IsRequired", typeof(bool), typeof(LabelWithRequiredInfo), new PropertyMetadata(false));
+        static LabelWithRequiredInfo()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(LabelWithRequiredInfo), new FrameworkPropertyMetadata(typeof(LabelWithRequiredInfo)));
+        }
+    }
+}
---
+++ ProjectMay/RequiredFieldValidationRule.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace ProjectMay
+{
+    //SOURCE: https://stackoverflow.com/questions/33256045/wpf-validation-depending-on-required-not-required-field/33385575
+    class RequiredFiedValidationRule : ValidationRule
+    {
+        public bool IsRequired { get; set; }
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            var content = value.ToString();
+            if (content != null)
+            {
+                if (IsRequired && string.IsNullOrWhiteSpace(content))
+                    return new ValidationResult(false, "Required content");
+            }
+            return ValidationResult.ValidResult;
+        }
+    }
+}
