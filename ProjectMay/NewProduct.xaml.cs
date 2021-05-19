0083abc1e4a0f48ad91f611bed185cc9326794b4
Tim-Audenaert <tim.audenaert@hotmail.be>
2021-05-19 15:57:54 +0200

    Testing JSon parsing

--- ProjectMay/NewProduct.xaml.cs
+++ ProjectMay/NewProduct.xaml.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -34,8 +37,7 @@ namespace ProjectMay
         private void AddProduct(object sender, RoutedEventArgs e)
         {
             using(Context ctx = new Context())
-            {
-                                                                                                                                    //TEMPORARY - WHAT IF ORDER CHANGES?      
+            {                                                                                                                                    //TEMPORARY - WHAT IF ORDER CHANGES?      
                 ctx.Products.Add(new Product(TxtName.Text, Convert.ToDouble(TxtPrice.Text), ctx.Suppliers.FirstOrDefault(s => s.Id == CmbSuppliers.SelectedIndex + 1)));
                 ctx.SaveChanges();
             }
@@ -44,6 +46,36 @@ namespace ProjectMay
 
         private void ImportJSon(object sender, RoutedEventArgs e)
         {
+            Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
+            // Set filter for file extension and default file extension 
+            dlg.DefaultExt = ".json";
+            dlg.Filter = "JSON Files (*.json)|*.json";
+
+
+            // Display OpenFileDialog by calling ShowDialog method 
+            Nullable<bool> result = dlg.ShowDialog();
+
+            string filename = string.Empty;
+            // Get the selected file name and display in a TextBox 
+            if (result.HasValue && result.Value)
+            {
+                // Open document                
+                filename = dlg.FileName;
+            }
+
+            //JObject o1 = JObject.Parse(File.ReadAllText(filename));
+            //Product product = o1.ToObject<Product>();
+            //CmbSuppliers.Text = product.Supplier.Name;
+            //TxtName.Text = product.Name;
+            //TxtPrice.Text = product.Price.ToString();
+
+            //read JSON directly from a file
+            using (StreamReader file = File.OpenText(filename))
+            using (JsonTextReader reader = new JsonTextReader(file))
+            {
+                JObject o2 = (JObject)JToken.ReadFrom(reader);
+                MessageBox.Show(o2.ToString());
+            }
 
         }
     }
