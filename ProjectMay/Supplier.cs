a00b00ee78ba01f364b8ad8f0921eb776e7dab32
Tim-Audenaert <tim.audenaert@hotmail.be>
2021-05-28 15:59:23 +0200

    Experimenting with abstract classes

--- ProjectMay/Supplier.cs
+++ ProjectMay/Supplier.cs
@@ -19,14 +19,14 @@ namespace ProjectMay
         
         }
 
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public string Address { get; set; }
-
         public override string ToString()
         {
             return Name;
         }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
     }
    
 }
